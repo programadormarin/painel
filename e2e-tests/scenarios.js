e410cbffa2af0fdb360918043c3993f31d3d79f1
Thiago Paes <mrprompt@gmail.com>
2015-09-30 03:26:44 -0300

    corrigindo testes

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -20,4 +20,88 @@ describe('my app', function () {
           toMatch(/.?/);
     });
   });
+
+  describe('aviso', function () {
+    beforeEach(function () {
+      browser.get('/#/aviso');
+    });
+
+
+    it('should render aviso when user navigates to /aviso', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('empregos', function () {
+    beforeEach(function () {
+      browser.get('/#/empregos');
+    });
+
+
+    it('should render empregos when user navigates to /empregos', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('empresa', function () {
+    beforeEach(function () {
+      browser.get('/#/empresa');
+    });
+
+
+    it('should render empresa when user navigates to /empresa', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('equipe', function () {
+    beforeEach(function () {
+      browser.get('/#/equipe');
+    });
+
+
+    it('should render equipe when user navigates to /equipe', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('login', function () {
+    beforeEach(function () {
+      browser.get('/#/login');
+    });
+
+
+    it('should render login when user navigates to /login', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('parceiros', function () {
+    beforeEach(function () {
+      browser.get('/#/parceiros');
+    });
+
+
+    it('should render parceiros when user navigates to /parceiros', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
+
+  describe('slides', function () {
+    beforeEach(function () {
+      browser.get('/#/slides');
+    });
+
+
+    it('should render slides when user navigates to /slides', function () {
+      expect(element.all(by.css('[ng-view] h4')).first().getText()).
+          toMatch(/.?/);
+    });
+  });
 });
