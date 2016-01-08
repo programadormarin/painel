def502435814ec0d7cb200fd2345cdc2e789594e
Thiago Paes <mrprompt@gmail.com>
2016-01-08 16:46:29 -0200

    Update 2016-01-08T18:46:23.622Z

--- app/custom.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function () {
-    $('.navbar-fixed-top').removeClass('hidden');
-    $('.container-fluid').removeClass('hidden');
-}, false);
