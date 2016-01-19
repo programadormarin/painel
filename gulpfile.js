07727bb53b743e085fef5a822b1d9954fbffe1a3
Thiago Paes <mrprompt@gmail.com>
2016-01-19 20:46:27 -0200

    produto "quero uma loja" a partir de agora

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('build/vendor'));
 
     gulp.src(['app/index.html'])
-        .pipe(replace('http://127.0.0.1:8081', 'https://api.mrprompt.com.br'))
+        .pipe(replace('http://127.0.0.1:8081', 'https://api.queroumaloja.net'))
         .pipe(minifyHTML({conditionals: true, spare:true}))
         .pipe(gulp.dest('build'));
 });
